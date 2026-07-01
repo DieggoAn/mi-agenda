085e2e4897bdd2407968e30a34753c941e1f95c5
DieggoAn <89940535+DieggoAn@users.noreply.github.com>
2026-07-01 10:49:36 -0400

    Modify SUPABASE_URL and enhance getHeaders function
    Updated SUPABASE_URL to include '/rest/v1' and modified getHeaders to include 'Prefer' header.

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+export const SUPABASE_URL = `${import.meta.env.VITE_SUPABASE_URL}/rest/v1`; 
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const getHeaders = () => {
-  return {
-    'Content-Type': 'application/json',
-    'apikey': SUPABASE_ANON_KEY,
-    'Authorization': `Bearer ${SUPABASE_ANON_KEY}`
-  };
-};
+
+export const getHeaders = () => ({
+  "Content-Type": "application/json",
+  "apikey": SUPABASE_ANON_KEY,
+  "Authorization": `Bearer ${SUPABASE_ANON_KEY}`, 
+  "Prefer": "return=representation"
+});
